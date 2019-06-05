a74bb5475c5d83019d0954ccf6eb1ef5bc3c0426
Chinmay <madeshichinmay@gmail.com>
2019-06-06 03:26:44 +0530

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const App=()=>{
+    return (
+        <div>App</div>
+    );
+}
+export default App
+
+
+/*
+
+Brief Outline
+App contains :
+1. SongList
+2.SongDetail
+
+Redux :
+
+1.REDUCERS : these store the current state data
+and help in changing the state.
+
+The reducer is a pure function that takes the 
+previous state 
+and an action, and returns the next state.
+
+SongListReducer
+SelectedSongReducer
+
+2.Action CREATORS: these help in changing state
+
+SelectSong
+
+
+
+ */
